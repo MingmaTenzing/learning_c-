90a458c7eadea233997040f5869f473086802317
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-05-07 14:14:26 +0800

    reference

---
+++ w3/reference.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string food = "Pizza";
+    string &meal = food;
+
+    cout << "this is food : " << food << endl;
+    cout << " this is meal : " << meal << endl;
+
+    meal = "rice";
+    cout << "after changing value of meal the value of food is : " << food << endl;
+
+    food = "dall";
+    cout << meal;
+
+    cout << food;
+    return 0;
+}
